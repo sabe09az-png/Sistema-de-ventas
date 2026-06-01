234de13e5496f61c24e278a5a873aa087e16c655
sabe09az-png <sabe09az@gmail.com>
2026-06-01 09:21:27 -0600

    Configuración para Clever Cloud con PostgreSQL

---
+++ GameDrop/settings_prod.py
@@ -0,0 +1,18 @@
+from .settings import *
+import os
+import dj_database_url
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+# Configuración de base de datos usando la variable POSTGRESQL_ADDON_URI
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('POSTGRESQL_ADDON_URI'))
+}
+
+# Archivos estáticos
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Seguridad
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', SECRET_KEY)
